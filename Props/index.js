f3203e4e36699b217a685ecec319f63845dfe69f
Rishi <114659322+rishimaheshwari123@users.noreply.github.com>
2023-02-04 12:16:39 +0530

    Create index.js

---
+++ Props/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDom from 'react-dom';
+import Card from "./Card";
+
+ReactDom.render(
+    <>
+    <Card imgsrc ="http://wallpapercave.com/wp/wp4056410.jpg"
+    title="A orignal series"
+    sname ="DARK"
+    link ="http://www.netflix.com/in/title/80990668?source=35"
+    />
+
+    <Card imgsrc ="http://wallpapercave.com/wp/wp4056410.jpg"
+    title="A orignal series"
+    sname ="Pathan"
+    link ="http://www.netflix.com/in/title/80990668?source=35"
+    />
+
+    <Card imgsrc ="http://wallpapercave.com/wp/wp4056410.jpg"
+    title="A orignal series"
+    sname ="Avenger"
+    link ="http://www.netflix.com/in/title/80990668?source=35"
+    />
+    
+    </>
+,document.getElementById('root'));

e4a2e26b8a1647595d88da28a34ae1eb47f80f2d
Rishi <114659322+rishimaheshwari123@users.noreply.github.com>
2023-02-05 09:54:53 +0530

    Update index.js

--- Props/index.js
+++ Props/index.js
@@ -2,40 +2,21 @@ import React from "react";
 import ReactDom from 'react-dom';
 import Card from "./Card";
 import Sdata from './Sdata';
+
+function nCard(val){
+    return(
+        <Card 
+        imgsrc={val.imgsrc}
+        title={val.title}
+        sname ={val.sname}
+        link ={val.link}
+        />
+    )
+}
+
 ReactDom.render(
     <>
     <h1>List of movie</h1>
-    <Card 
-    imgsrc={Sdata[0].imgsrc}
-    title={Sdata[0].title}
-    sname ={Sdata[0].sname}
-    link ={Sdata[0].link}
-    />
-
-    <Card
-    imgsrc={Sdata[1].imgsrc}
-    title={Sdata[1].title}
-    sname ={Sdata[1].sname}
-    link ={Sdata[1].link}
-    />
-    <Card
-    imgsrc={Sdata[2].imgsrc}
-    title={Sdata[2].title}
-    sname ={Sdata[2].sname}
-    link ={Sdata[2].link}
-    />
-    <Card
-    imgsrc={Sdata[3].imgsrc}
-    title={Sdata[3].title}
-    sname ={Sdata[3].sname}
-    link ={Sdata[3].link}
-    />
-    <Card
-    imgsrc={Sdata[4].imgsrc}
-    title={Sdata[4].title}
-    sname ={Sdata[4].sname}
-    link ={Sdata[4].link}
-    />
-    
+    {Sdata.map(nCard)}  
     </>
 ,document.getElementById('root'));
